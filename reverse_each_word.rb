06ae3fd608cb510ae0959eaf8f0437862b316ada
Maasa Kono <konomaasa@gmail.com>
2019-06-21 02:24:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(sentence)
+  reversed = []
   sentence.split.each do |word|
-    puts word.reverse!
+    reversed << word.reverse!
   end
+  reversed.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
